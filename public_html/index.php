ef5739f9869e83ce264d14a4704ec6282ea171fa
Alex-Nabu <king.zan@hotmail.com>
2014-07-13 21:20:16 -0500

    better comments and formating plus added config variable

--- public_html/index.php
+++ public_html/index.php
@@ -1,20 +1,36 @@
 <?php
 
 /*
+ * --------------------------------INITIAL SETUP-------------------------------
+ * 
+ * The user points to the directory that conatins their files. A simple check
+ * is done to varify that the directory is in fact correct and the main config
+ * file can be read.
+ * ----------------------------------------------------------------------------
+ */
 
---------------------------------INITIAL DOCUMENTATION--------------------------------
+ 	// Edit only this
+	$core_directory='../../SimpleMvC/';
+	
+	// Try to load the config file
+	if(!file_exists($core_directory.'config/config.php'))
+	exit("Path to main config not set. Please set the path to the  core_directory in the file ".__FILE__);
+	
 
-THIS IS GROUND ZERO FOR THE FRAMWORK. AFTER THE HTCACCESS FILE ROUTES THE USER TO THIS
-FILE IT INITIALISES SESSIONS AND GETS THE MAIN CONFIG FILE. AFTER WHICH THE USERS ACTION
-IS PARSED BY OUR ROUTER ACTION IT THEN CONTINUES TO CREATE AN INSTANCE OF OUR OBJECT FACTORY
-TO BUILD WHATWEVER CONTROLLER WAS INITIATED BY THE USERS REQUEST
-
--------------------------------------------------------------------------------------
-
-*/
-
-session_start();
-require_once '../SimpleMvC/config/config.php';
+/*
+ * -------------------------INITIALISE THE APPLICATION-------------------------
+ * 
+ * This is ground zero for the framwork. After the htcaccess file routes the user
+ * to this file it initialises sessions and gets the main config file. after 
+ * which the users action is parsed by our router it then continues to create
+ * an instance of our object factory to build whatwever controller was initiated 
+ * by the users request
+ * ----------------------------------------------------------------------------
+ */
+ 
+ 
+	session_start();
+	require_once '../SimpleMvC/config/config.php';
 
 	$route=isset($_GET['action'])?new router_controller($_GET['action']):new router_controller($_GET['action']="/index");
 	$route->parse_route();
