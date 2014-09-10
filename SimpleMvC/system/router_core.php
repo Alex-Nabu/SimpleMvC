3bfd014d18b3ef8623a1452c2398b3c65db5e283
Alex <king.zan@hotmail.com>
2014-09-10 13:56:32 -0400

    cleaned router

--- SimpleMvC/system/router_core.php
+++ SimpleMvC/system/router_core.php
@@ -4,6 +4,7 @@ class router_core
 {
 	
 	private $request;
+	private $request_method;
 	private $controller;
 	private $object_factory;
 	private $plugin_manager;
@@ -11,41 +12,43 @@ class router_core
 	public function __construct(object_factory_core $factory, $request)
 	{
 		$this->object_factory=$factory;
+		
 		$this->request=$request;
+		
 		$this->plugin_manager=$this->object_factory->build_plugin_manager();
+		
+		$this->request_method=$_SERVER['REQUEST_METHOD'] == 'POST' ? 'POST' : 'GET';
 	}
 	
 	
 	// Load routing plugin or use default router
-	// Default router uses uri name plus http verb to match match controller naming convention
+	// Default router uses uri, http verb to match match controller naming convention
 	public function get_controller()
 	{
 		
 		if($this->plugin_manager->plugin_loaded("routing"))
 		{
-			return $this->controller=$this->plugin_manager->_plugin("routing");
+			$this->controller=$this->plugin_manager->_plugin("routing");
+			return $this->controller;
 		}
 		else
 		{
 			$this->request=ltrim($this->request,'/');
 			$this->request=rtrim($this->request,'/');
+			$this->controller=$this->request;
 			
-			switch ($_SERVER['REQUEST_METHOD'])
+			switch($this->request_method)
 			{
-				case'GET' :
-					
-				$this->controller=$this->request.'_page';
+				case 'GET':
+				$this->controller.='_page';
 				break;
 				
 				case'POST':
-					
-				$this->controller=$this->request.'_form';
+				$this->controller.='_form';
 				break;
 				
 				default:
-				
-				$this->controller=$this->request.'_page';
-				break;
+				$this->controller.='_page';
 			}
 			
 			return $this->controller;
