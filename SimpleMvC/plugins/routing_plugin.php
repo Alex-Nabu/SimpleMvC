979cd65e6086e588f47f5f6de3b48986e8feccf0
Alex <king.zan@hotmail.com>
2014-09-15 05:04:02 -0400

    added altoRouter plugin

--- SimpleMvC/plugins/routing_plugin.php
+++ SimpleMvC/plugins/routing_plugin.php
@@ -1,10 +1,52 @@
 <?php
 
+// Interface with AltoRouter to map uris to controllers
 function rewrite_uri($args)
 {
-	print_r($args);
+	$request = $args['request'];
 	
-	return 'index_page';
+	$request_method = $args['method'];
+	
+	$AltoRouter = new AltoRouter_plugin();
+	
+	// If we have a routes file load them
+	if(is_file(core_directory.'/plugins/routes.php'))
+	{
+			include(core_directory.'/plugins/routes.php');
+			
+			if(isset($routes) && is_array($routes))
+			{
+				$routes=& $routes;
+			}
+			else
+			{
+				exit("Invalid routes file");
+			}
+
+	}
+	else
+	{
+		exit("Invalid routes file");
+	}
+	
+	$AltoRouter->addRoutes($routes);
+	
+	$mathes = $AltoRouter->match($request, $request_method);
+	
+	if(empty($mathes))
+	return '404_page';
+	
+	// If the we have any params from the url
+	if(!empty($mathes['params']))
+	{
+		foreach ($mathes['params'] as $key => $value) 
+		{
+			$_GET[$key] = $value;
+		}
+	}
+	
+	return $mathes['target'];
+	
 }
 
 ?>
