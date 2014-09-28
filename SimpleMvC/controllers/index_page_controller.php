6e9c3c3ded94167f3e079a46f5ca6f683dbabb95
Alex-Nabu <king.zan@hotmail.com>
2014-09-27 20:20:19 -0500

    added comments

--- SimpleMvC/controllers/index_page_controller.php
+++ SimpleMvC/controllers/index_page_controller.php
@@ -1,6 +1,7 @@
 <?php
 namespace SimpleMvC\controllers;
 
+
 /**
  * @todo Update with comments to use for example controller
  * 
@@ -12,27 +13,27 @@ use SimpleMvC\system\Controller_Exception;
 
 class index_page_controller implements \SimpleMvC\system\controller_interface
 {
-		
-protected $parameters=array();
-protected $view_template=array('header'=>'index_header','body'=>'index_body','footer'=>'index_footer');
-protected $object_factory;
-protected $model;
-private $model_args=array();
-protected $view;
-private $view_args;
+
+ protected $view_template=array('header'=>'index_header','body'=>'index_body','footer'=>'index_footer');
+ protected $object_factory;
+ protected $model;
+ protected $view;
 
  public function __construct($factory)
  {
  	
-	$this->object_factory=$factory;
+ 	$this->object_factory=$factory; // Store object factory
+	$this->object_factory->build_model('authentication', array()); // Create a model
 	
  }
   
   
-  public function _execute()  
+  public function _execute()  // Core function to run
   {
-		$this->view=$this->object_factory->build_view($this->view_template, $this->view_args);
-		$this->view->render();
+  	
+  	$this->view=$this->object_factory->build_view($this->view_template, $this->view_args);
+  	$this->view->render();
+	
   }
 
 }
