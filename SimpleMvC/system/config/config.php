a695cb02ca05bf0f76ae1f7ce7e3595cae7dacf6
Alex-Nabu <king.zan@hotmail.com>
2014-12-23 21:00:18 -0500

    steps toward making a fully pluggeble api

--- SimpleMvC/system/config/config.php
+++ SimpleMvC/system/config/config.php
@@ -1,6 +1,12 @@
 <?php
 namespace SimpleMvC\config;
 
+/**
+ * @todo Complete this object's interface
+ * 
+ * @todo figure out a way to allow the methods to be overloaded throught this plugins api
+ */
+
 class config
 {
 	public $factory;
@@ -10,11 +16,8 @@ class config
 	public function __construct()
 	{
 		
-		// Require in the autoloader
-		require_once(core_directory.'/system/autoloader.php');
+		$this->init_plugin_manager();
 		
-		// Register the autoloader function from the system Namespace
-		spl_autoload_register('\SimpleMvC\system\autoloader');
 		
 		$this->factory = new \SimpleMvC\system\object_factory;
 		
@@ -22,6 +25,29 @@ class config
 		
 		$this->load_controller_exceptions();	
 			
+	}
+	
+	
+	public function init_plugin_manager()
+	{
+		require_once(core_directory.'/system/plugin_manager.php');
+		$this->plugin_manager = new \SimpleMvC\system\plugin_manager();
+		
+		// Tell all the plugins of the system start event
+		$this->plugin_manager->_hook('system_start', $this);
+	}
+	
+	
+	public function init_autoloader()
+	{
+		// Require in the autoloader
+		require_once(core_directory.'/system/autoloader.php');
+		
+		// Register the autoloader function from the system Namespace
+		spl_autoload_register('\SimpleMvC\system\autoloader');
+		
+		// Tell all the plugins of auto_loader init
+		$this->plugin_manager->_hook('init_autoloader', $this);
 	}
 	
 	
