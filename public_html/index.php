230e586eca10c4ba7c273b6b52f4fcceffd18abb
Alex-Nabu <king.zan@hotmail.com>
2014-12-26 03:01:50 -0500

    refined ground zero

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,5 @@
 <?php
+namespace SimpleMvC\system;
 
 /*
  * --------------------------------INITIAL SETUP-------------------------------
@@ -29,54 +30,25 @@
 	exit("Path to main config not set. Please set the path  in the file ".__FILE__.". \r\n
 	      Your Config file is currently initialized as ".$main_config);
 	
-	
+		
 /*
  * -------------------------INITIALISE THE APPLICATION-------------------------
  * 
- * This is ground zero for the framework. After the htcaccess file routes the user
- * to this file it initializes sessions and gets the main config file. after 
- * which the users action is parsed by our router it then continues to create
- * an instance of our object factory to build whatever controller was initiated 
- * by the users request
+ * This is ground zero for the framework.
  * ----------------------------------------------------------------------------
  */
 
- 
-	session_start();
-	
+	// Require the main config
 	require_once($main_config);
 	
-	$config = new \SimpleMvC\config\config;
+	// Instanciate the main configuration object
+	$config = new \SimpleMvC\config\config();
+	 
+	// Instanciate the framework
+	$app = new SimpleMVC($config);
 	
-	// Instanciate the object factory
-	// Used to create other objects
-	$object_factory = $config->factory;
+	// Run the app
+	$app->_run();
 	
-	// Default uri to index if none provided
-	$uri=isset($_GET['action'])?$_GET['action']:'index';
-	
-	// Instanciate the router
-	// Default to index controller if no uri present
-	$router=$object_factory->build_router($uri);
-	
-	try
-	{
-			
-		// Instanciate the controller
-		// Takes a string with the controller name
-		$controller=$object_factory->build_controller($router->get_controller());
-			
-		$controller->_execute();
-		
-	}
-	catch(\SimpleMvC\system\Controller_Exception  $exception)
-	{
-		
-		if($exception->return_url() !== '') header('location:'.$exception->return_url());
-		
-		error_log($exception->__toString());
-		exit;
-		
-	}
 	
 ?>
