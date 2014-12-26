d9a3e78fb24c4e74e68434f47f7c7619df2e5740
Alex-Nabu <king.zan@hotmail.com>
2014-12-26 03:00:20 -0500

    fixed namespace

--- SimpleMvC/system/config/config.php
+++ SimpleMvC/system/config/config.php
@@ -1,5 +1,5 @@
 <?php
-namespace SimpleMvC\config;
+namespace SimpleMvC\system\config;
 
 /**
  * @todo Complete this object's interface
@@ -12,54 +12,70 @@ class config
 	public $factory;
 	public $autoloader;
 	public $plugin_manager;
+	public $autoloader_dir;
 	
 	public function __construct()
 	{
 		
+		// set the directory of the autoloader
+		$this->autoloader_dir = core_directory.'/system/autoloader.php';
+		
+		// start the plugin manager
 		$this->init_plugin_manager();
+				
+		// Set and register our autoloader file
+		$this->init_autoloader();
+		
+		// Create and set and instance of the object factory
+		$this->init_object_factory();
 		
 		
-		$this->factory = new \SimpleMvC\system\object_factory;
-		
-		$this->plugin_manager = $this->factory->build_plugin_manager();
-		
-		$this->load_controller_exceptions();	
-			
 	}
 	
 	
 	public function init_plugin_manager()
 	{
+		
 		require_once(core_directory.'/system/plugin_manager.php');
 		$this->plugin_manager = new \SimpleMvC\system\plugin_manager();
 		
 		// Tell all the plugins of the system start event
-		$this->plugin_manager->_hook('system_start', $this);
+		$this->plugin_manager->_hook('system_start', array("config"=>$this));
 	}
 	
 	
 	public function init_autoloader()
 	{
+		// Tell all the plugins hooked to this event that it occured.
+		$this->plugin_manager->_hook('pre_autoloader_init', array("config"=>$this));
+		
 		// Require in the autoloader
-		require_once(core_directory.'/system/autoloader.php');
+		require_once($this->autoloader_dir);
 		
 		// Register the autoloader function from the system Namespace
 		spl_autoload_register('\SimpleMvC\system\autoloader');
 		
-		// Tell all the plugins of auto_loader init
-		$this->plugin_manager->_hook('init_autoloader', $this);
+		
+		// Tell all the plugins hooked to this event that it occured.
+		$this->plugin_manager->_hook('post_autoloader_init', array("config"=>$this));
+		
 	}
 	
 	
-	public function load_controller_exceptions()
+	public function init_object_factory()
 	{
-		// $this->plugin_manager->hook("controller_exceptions");
+		// Tell all the plugins hooked to this event that it occured.
+		$this->plugin_manager->_hook('pre_factory_init', array("config"=>$this));
 		
-		// Load up controller exceptions file
-		require_once(core_directory.'/system/controller_exception.php');
+		// Create and set the object factory
+		$this->factory = new \SimpleMvC\system\object_factory;
+		
+		// Tell all the plugins hooked to this event that it occured.
+		$this->plugin_manager->_hook('post_factory_init', array("config"=>$this));
+		
 		
 	}
-	
+		
 }
  
 
