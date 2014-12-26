7ff3d03621e21a946adf6a427a646d46e2fb03cf
Alex-Nabu <king.zan@hotmail.com>
2014-12-26 02:59:54 -0500

    added core app object file

---
+++ SimpleMvC/system/SimpleMvC.php
@@ -0,0 +1,18 @@
+<?php
+namespace SimpleMvC\system;
+
+class SimpleMvC
+{
+	
+	public $plugin_manager;
+	public $object_factory;
+	public $config;
+	
+	public function __construct($config)
+	{
+		// Start doing stuff. Remeber first thing needs to be pre_app_init_hook
+	}
+	
+}
+
+?>
